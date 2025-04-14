f4cc7ad5b30544347307c9ae73d1c884ca307f7f
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-04-14 22:33:19 +0600

    update

--- src/modules/_subscription/userSubscription/userSubscription.model.ts
+++ src/modules/_subscription/userSubscription/userSubscription.model.ts
@@ -1,65 +1,82 @@
 import { model, Schema } from 'mongoose';
 import paginate from '../../../common/plugins/paginate';
 import { UserSubscriptionStatusType } from './userSubscription.constant';
-import { IUserSubscription, IUserSubscriptionModel } from './userSubscription.interface';
+import {
+  IUserSubscription,
+  IUserSubscriptionModel,
+} from './userSubscription.interface';
 import { RenewalFrequncyType } from '../subscription/subscription.constant';
 
 const userSubscriptionSchema = new Schema<IUserSubscription>(
   {
-    userId: { //🔗
+    userId: {
+      //🔗
       type: Schema.Types.ObjectId,
       ref: 'User',
       required: [false, 'User Id is required'],
     },
-    subscriptionId: { //🔗
+    subscriptionId: {
+      //🔗
       type: Schema.Types.ObjectId,
       ref: 'Subscription',
       required: [false, 'Subscription Id is required'],
     },
-    subscriptionStartDate : {
-        type: Date,
-        required: true,
-        validate : {
-          validator : (value) => value <=  new Date(),
-          message : 'Subscription Start Date must be in the past'
-        }
+    subscriptionStartDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: value => value <= new Date(),
+        message: 'Subscription Start Date must be in the past',
+      },
     },
-    renewalDate : {
-        type: Date,
-        required: true,
-        validate:{
-          validator: function (value) {
-            return value > this.subscriptionStartDate;
-          }, 
-          message : 'initial Period End Date must be after subscription start date ', 
+    subscriptionEndDate: {
+      type: Date,
+      required: false,
+      validate: {
+        validator: function (value) {
+          return value > this.subscriptionStartDate;
         },
+        message: 'Subscription end date must be after subscription start date',
+      },
     },
-    currentPeriodStartDate : { // renewal period end date
-        type: Date,
-        required: true,
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > this.subscriptionStartDate;
+        },
+        message:
+          'initial Period End Date must be after subscription start date ',
+      },
     },
-    billingCycle : {
-        type : Number,
-        required: [true, 'billingCycle is required'],
-        default : 1,
+    currentPeriodStartDate: {
+      // renewal period end date
+      type: Date,
+      required: true,
     },
-    isAutoRenewed : {
+    billingCycle: {
+      type: Number,
+      required: [true, 'billingCycle is required'],
+      default: 1,
+    },
+    isAutoRenewed: {
       type: Boolean,
       required: [false, 'isAutoRenewed is not required'],
       default: true,
     },
-    cancelledAt : {
-        type: Date,
-        required: [false, 'cancelledAt is not required'],
-        default: null,
+    cancelledAt: {
+      type: Date,
+      required: [false, 'cancelledAt is not required'],
+      default: null,
     },
-    status : {
+    status: {
       type: String,
-      enum : [
-          UserSubscriptionStatusType.freeTrial,
-          UserSubscriptionStatusType.active,
-          UserSubscriptionStatusType.expired,
-          UserSubscriptionStatusType.cancelled,	
+      enum: [
+        UserSubscriptionStatusType.freeTrial,
+        UserSubscriptionStatusType.active,
+        UserSubscriptionStatusType.expired,
+        UserSubscriptionStatusType.cancelled,
       ],
       required: [true, 'status is required'],
     },
@@ -81,18 +98,21 @@ const userSubscriptionSchema = new Schema<IUserSubscription>(
       default: false, // Indicates if the free trial has been converted to a paid subscription
     },
 
-    stripe_subscription_id : {
-        type: String,
-        required: [true, 'stripe_subscription_id is required'],
-        default: null,
+    stripe_subscription_id: {
+      type: String,
+      required: [true, 'stripe_subscription_id is required'],
+      default: null,
     },
-    external_customer_id : {
-        // > stripe er customer id ...
-        type: String,
-        required: [true, 'stripe_customer_id or external_customer_id is required'],
-        default: null,
+    external_customer_id: {
+      // > stripe er customer id ...
+      type: String,
+      required: [
+        true,
+        'stripe_customer_id or external_customer_id is required',
+      ],
+      default: null,
     },
-    isDeleted : {
+    isDeleted: {
       type: Boolean,
       required: [false, 'isDeleted is not required'],
       default: false,
@@ -103,44 +123,44 @@ const userSubscriptionSchema = new Schema<IUserSubscription>(
 
 userSubscriptionSchema.plugin(paginate);
 
-// auto calculate the renewal date if its not provided ... 
-userSubscriptionSchema.pre('save', function(next) {
+// auto calculate the renewal date if its not provided ...
+userSubscriptionSchema.pre('save', function (next) {
   // Rename _id to _projectId
   //this._taskId = this._id;
   //this._id = undefined;  // Remove the default _id field
 
-  if(!this.renewalDate){
+  if (!this.renewalDate) {
     const renewalPeriods = {
       daily: 1,
-      weekly : 7,
-      monthly: 30, 
-      yearly : 365 
-    }
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
 
     this.renewalDate = new Date(this.subscriptionStartDate);
-    this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.renewalFrequncy])
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriods[this.renewalFrequncy]
+    );
   }
 
-  // auto update the status if  renewal date has passed 
-  if(this.renewalDate < new Date()){
-    this.status = UserSubscriptionStatusType.expired
+  // auto update the status if  renewal date has passed
+  if (this.renewalDate < new Date()) {
+    this.status = UserSubscriptionStatusType.expired;
   }
 
   next();
 });
 
-
 // Use transform to rename _id to _projectId
 userSubscriptionSchema.set('toJSON', {
   transform: function (doc, ret, options) {
-    ret._userSubscriptionId = ret._id;  // Rename _id to _userSubscriptionId
-    delete ret._id;  // Remove the original _id field
+    ret._userSubscriptionId = ret._id; // Rename _id to _userSubscriptionId
+    delete ret._id; // Remove the original _id field
     return ret;
-  }
+  },
 });
 
-
-export const UserSubscription = model<IUserSubscription, IUserSubscriptionModel>(
-  'UserSubscription',
-  userSubscriptionSchema
-);
+export const UserSubscription = model<
+  IUserSubscription,
+  IUserSubscriptionModel
+>('UserSubscription', userSubscriptionSchema);
