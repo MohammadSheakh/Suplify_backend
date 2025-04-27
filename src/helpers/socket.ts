506c9fe93601453f73b260ae3dd908fb753eac0f
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-04-27 23:26:49 +0600

    ,

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -12,9 +12,11 @@ declare module 'socket.io' {
 
 const socket = (io: Server) => {
   io.on('connection', (socket: Socket) => {
-    // we dont need this console anymore .. 
-    logger.info(colors.blue('🔌🟢 A user connected .. WS Connection ...'));
-    socket.on('user-connected', (userId: string) => {
+    // we dont need this console anymore ..
+    logger.info(
+      colors.blue(`🔌🟢 1️⃣ A user connected .. socket id is : ${socket.id} `)
+    );
+    socket.on('user-connected 2️⃣', (userId: string) => {
       socket.userId = userId;
       socket.join(userId); // Join the room for the specific user
       logger.info(
@@ -22,67 +24,83 @@ const socket = (io: Server) => {
       );
     });
 
-    // Join a room for a specific conversation // Join Chat Room ..  
-    socket.on('join-room', (conversationId)  => {
-      console.log(`User joined in Conversation : ${conversationId}`);
+    // Join a room for a specific conversation // Join Chat Room ..
+    socket.on('join-room', conversationId => {
+      console.log(`User 3️⃣ joined in Conversation : ${conversationId}`);
       socket.join(conversationId); // Join a room based on conversationId
 
-      // welcome current user .. 
-      socket.emit('message', formatMessage('System','welcome to chatcord ')) // single user
+      console.log('conversationId 🚧', conversationId);
 
-      // broadcast to all user when a user connects .. 
+      // welcome current user ..
+      socket.emit(
+        'message',
+        formatMessage('System', 'welcome 4️⃣ to chatcord ')
+      ); // single user
+
+      // broadcast to all user when a user connects ..
+
+      /*
+       * socket.broadcast
+       * .to(conversationId)
+       * .emit('message', formatMessage('System', 'A user has joined the chat')); // single user
+       *
+       */
 
       /*** Conversation Id er upor base kore .. user er information niye ashte hobe .. */
 
-      socket.to(conversationId).emit('message', formatMessage('System','A user has joined the chat'));
+      socket
+        .to(conversationId)
+        .emit(
+          'message',
+          formatMessage('System', 'A user 5️⃣ has joined the chat')
+        );
     });
 
-    // welcome current user .. 
-    socket.emit('message', formatMessage('System','welcome to chatcord ')) // single user
-
-    // Broadcast when a user connects without the user who connecting .. 
-    /**
-     *  socket.broadcast.emit('message' , formatMessage('System','A user has joined the chat')); 
-     * 
-     * // later we will have actual user names ..  
-     */
-    
-  
-    socket.broadcast.emit('message' , formatMessage('System','A user has joined the chat')  /*socket.userId*/);
-    // later we will have actual user names ..  
-
-
-    // to send message everybody // all the clients in general .. 
-    // io.emit()
-
-    // 🟢 ------ From front-end we emit chatMessage .. we have to catch that .. 
-    // Listen for chatMessage  
-
-    /*
-    socket.on('chatMessage', (message: string) => {
-      console.log(`Message received: ${message}`);
-      // Broadcast the message to all users in the room
-      // emit back to the client .. 
-      io.emit('message', formatMessage('USER', message));
-    });
-    */  
-
-    socket.on("send-message", (data) => {
-      console.log(`New Data received for send-message event : ${data}`);
+    // 🟢 ------ From front-end we emit chatMessage .. we have to catch that ..
+    // Listen for chatMessage
+    socket.on('send-message', data => {
+      console.log(`New Data 9️⃣ received for send-message event : ${data}`);
+      console.log('socket.id 🚧', socket.id);
       const eventName = `messages::${data.conversationId}`;
+      console.log('eventName 🚧', eventName);
       io.to(data.conversationId).emit(eventName, data.message);
     });
 
+    // welcome current user ..
+    socket.emit('message', formatMessage('System', 'welcome 6️⃣ to chatcord ')); // single user
+
+    // Broadcast when a user connects without the user who connecting ..
+    /**
+     *  socket.broadcast.emit('message' , formatMessage('System','A user has joined the chat'));
+     *
+     * // later we will have actual user names ..
+     */
+
+    socket.broadcast.emit(
+      'message',
+      formatMessage(
+        'System',
+        'A  7️⃣ user has joined the chat'
+      ) /*socket.userId*/
+    );
+    // later we will have actual user names ..
+
+    // to send message everybody // all the clients in general ..
+    // io.emit()
+
     // Leave a room when a user disconnects or leaves
     socket.on('leave-room', conversationId => {
-      console.log(`User left room: ${conversationId}`);
+      console.log(`User left 1️⃣0️⃣ room: ${conversationId}`);
       socket.leave(conversationId);
     });
 
-    // runs when a client disconnects ..  
+    // runs when a client disconnects ..
     socket.on('disconnect', () => {
-      // to let everyone know .. 
-      io.emit('message',  formatMessage('System','A user has left the chat') );
+      // to let everyone know ..
+      io.emit(
+        'message',
+        formatMessage('System', 'A 1️⃣1️⃣ user has left the chat')
+      );
       logger.info(colors.red(`🔌🔴 A user disconnected`));
     });
   });
