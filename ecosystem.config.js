65f2c84884b73a0de0d51fa8f97eb6df9cef3d74
MohammadSheakh <mohammad.sheakh@gmail.com>
2025-12-21 10:26:02 +0600

    updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,6 +23,7 @@ module.exports = {
       name: 'suplify-api',
       script: './src/serverV2.ts',
       interpreter: './node_modules/.bin/ts-node', // Use local ts-node (important!)
+      exec_interpreter: '/home/ubuntu/.nvm/versions/node/v18.19.1/bin/node',  // Full path to v18 binary
       interpreter_args: '--transpile-only',      // Pass args to ts-node
       cwd: './',                                 // Working directory
       instances: 1,
