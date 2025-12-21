915ded06858cbbd6fc773b4b01f7ab33d4ed2be3
MohammadSheakh <mohammad.sheakh@gmail.com>
2025-12-21 10:50:33 +0600

    ecosystem updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,25 @@
 // this file is for configuring PM2 process manager  // --- suplify
 // ecosystem.config.js
 
-// module.exports = {
-//   apps: [{
-//     name: 'suplify-api',
-//     interpreter: '/home/ubuntu/.nvm/versions/node/v18.19.1/bin/node',
-//     script: 'node_modules/.bin/ts-node',
-//     args: 'src/index.ts', // or your entry file
-//     instances: 1,
-//     autorestart: true,
-//     watch: false,
-//     max_memory_restart: '1G',
-//     env: {
-//       NODE_ENV: 'production'
-//     }
-//   }]
-// }
+/*---------------------------
+module.exports = {
+  apps: [{
+    name: 'suplify-api',
+    interpreter: '/home/ubuntu/.nvm/versions/node/v18.19.1/bin/node',
+    script: 'node_modules/.bin/ts-node',
+    args: 'src/index.ts', // or your entry file
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+}
+----------------------------*/
 
+/*-----------------------
 module.exports = {
   apps: [
     {
@@ -38,8 +41,28 @@ module.exports = {
     },
   ],
 };
+---------------------------------*/
 
 // -- to start this .. pnpm pm2 delete suplify-api
 // ---                 pnpm pm2 start ecosystem.config.js
 
+module.exports = {
+  apps: [
+    {
+      name: 'suplify-api',
+      script: './src/serverV2.ts',
+      interpreter: 'node',
+      interpreter_args: '--loader ts-node/esm/transpile-only',
+      cwd: process.cwd(),
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        TS_NODE_TRANSPILE_ONLY: 'true',
+      },
+    },
+  ],
+};
 
