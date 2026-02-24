7726fdcfe4227ae05b5a56570b52342e8c3185e2
MohammadSheakh <mohammad.sheakh@gmail.com>
2026-02-24 14:40:16 +0600

    discount logic updated for order for standard user

--- src/modules/order.module/order/order.service.ts
+++ src/modules/order.module/order/order.service.ts
@@ -70,7 +70,12 @@ export class OrderService extends GenericService<typeof Order, IOrder>{
 
         const usersSubscriptionToCalculateDiscount = await User.findById(user.userId).select('subscriptionType');
 
-        let discount = 10;
+        let discount = 0;
+
+        if(usersSubscriptionToCalculateDiscount.subscriptionType === TSubscription.standard){
+            discount = 10;
+        }
+
         if(usersSubscriptionToCalculateDiscount.subscriptionType === TSubscription.standardPlus){
             discount = 15;
         }
