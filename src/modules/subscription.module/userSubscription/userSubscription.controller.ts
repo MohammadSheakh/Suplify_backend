0e06d710519ee1f5c642da154438bd64bd496627
MohammadSheakh <mohammad.sheakh@gmail.com>
2026-05-08 22:05:00 -0600

    (userSubscription)  issue fixed for all user

--- src/modules/subscription.module/userSubscription/userSubscription.controller.ts
+++ src/modules/subscription.module/userSubscription/userSubscription.controller.ts
@@ -1,9 +1,12 @@
+import { config } from "../../../config";
+import ApiError from "../../../errors/ApiError";
 import catchAsync from "../../../shared/catchAsync";
 import omit from "../../../shared/omit";
 import pick from "../../../shared/pick";
 import sendResponse from "../../../shared/sendResponse";
 import { GenericController } from "../../_generic-module/generic.controller";
-import { IUser } from "../../token/token.interface";
+import { IUser, TokenType } from "../../token/token.interface";
+import { TokenService } from "../../token/token.service";
 import { SubscriptionPlan } from "../subscriptionPlan/subscriptionPlan.model";
 import { IUserSubscription } from "./userSubscription.interface";
 import { UserSubscription } from "./userSubscription.model";
@@ -32,8 +35,49 @@ export class UserSubscriptionController extends GenericController<typeof UserSub
 
 
     getAllWithPaginationV2 = catchAsync(async (req: Request, res: Response) => {
-        //const filters = pick(req.query, ['_id', 'title']); // now this comes from middleware in router
-        const filters =  omit(req.query, ['sortBy', 'limit', 'page', 'populate']); ;
+        const tokenWithBearer = req.headers.authorization;
+        if (!tokenWithBearer) {
+            // throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
+
+            const subscription = await SubscriptionPlan.find({
+                isActive: true,
+                isDeleted: false
+            });
+
+            let result = {
+                results: [],
+                page: 1,
+                limit: 6,
+                totalPages: 0,
+                totalResults: 0
+            }
+
+            sendResponse(res, {
+                code: StatusCodes.OK,
+                data: {result, subscription},
+                message: `All ${this.modelName} with pagination`,
+                success: true,
+            });
+        }
+
+        if (tokenWithBearer.startsWith('Bearer')) {
+            const token = tokenWithBearer.split(' ')[1];
+        
+            // Step 2: Verify Token
+            const verifyUser = await TokenService.verifyToken(
+                token,
+                config.jwt.accessSecret as any,
+                TokenType.ACCESS
+            );
+
+            // Step 3: Attach user to the request object
+            req.user = verifyUser;
+        }
+
+        req.query.userId  = (req.user as any).userId
+
+
+        const filters =  omit(req.query, ['sortBy', 'limit', 'page', 'populate']); 
         const options = pick(req.query, ['sortBy', 'limit', 'page', 'populate']);
 
         // ✅ Default values
@@ -42,15 +86,27 @@ export class UserSubscriptionController extends GenericController<typeof UserSub
 
         // ✅ If middleware provided overrides → use them
         if (req.queryOptions) {
-        if (req.queryOptions.populate) {
-            populateOptions = req.queryOptions.populate;
-        }
-        if (req.queryOptions.select) {
-            select = req.queryOptions.select;
-        }
+            if (req.queryOptions.populate) {
+                populateOptions = req.queryOptions.populate;
+            }
+            if (req.queryOptions.select) {
+                select = req.queryOptions.select;
+            }
         }
 
+        
+
         const result = await this.service.getAllWithPagination(filters, options, populateOptions , select );
+
+        /*---------------
+        "result": {
+                "results": [],
+                "page": 1,
+                "limit": 6,
+                "totalPages": 0,
+                "totalResults": 0
+            }
+        -----------------*/
 
         const subscription = await SubscriptionPlan.find({
             isActive: true,
@@ -62,8 +118,8 @@ export class UserSubscriptionController extends GenericController<typeof UserSub
         data: {result, subscription},
         message: `All ${this.modelName} with pagination`,
         success: true,
+        });
     });
-  });
 
     // add more methods here if needed or override the existing ones
 }
--- src/modules/subscription.module/userSubscription/userSubscription.route.ts
+++ src/modules/subscription.module/userSubscription/userSubscription.route.ts
@@ -38,7 +38,7 @@ const controller = new UserSubscriptionController();
 router.route('/paginate').get(
   auth(TRole.common),
   validateFiltersForQuery(optionValidationChecking(['_id', 'userId'])),
-  getLoggedInUserAndSetReferenceToUser('userId'),
+  // getLoggedInUserAndSetReferenceToUser('userId'),
   setRequestFiltersV2({
     sortBy: '-createdAt',
     limit : 6,
