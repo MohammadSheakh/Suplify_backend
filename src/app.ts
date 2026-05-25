dd6241764a5ff8b31933470ecda0d691b0b6b6cc
MohammadSheakh <mohammad.sheakh@gmail.com>
2026-05-25 10:12:34 +0600

    x-time-zone headers added in the allowedHeaders

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,7 @@ app.use(
     credentials: true,
     
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-time-zone'],
   })
 );
 
